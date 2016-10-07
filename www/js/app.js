b9cdf43b64f88b2bde7583a288ad36ca10b4155b
Desarrollo <desarrollo@iMac-de-Desarrollo.local>
2016-10-07 10:46:03 -0300

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,357 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+
+
+      .state('app.inicio', {
+        url: '/inicio',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/inicio.html'
+          }
+        }
+      })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+
+      .state('app.playlistsMovie', {
+    url: '/playlistsMovie',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlistsMovie.html',
+        controller: "servicioMovieCtrl"
+      }
+    }
+  })
+      .state('app.playlistsNew', {
+        url: '/playlistsNew',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsNew.html',
+            controller: "servicioNewCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsTrailer', {
+        url: '/playlistsTrailer',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsTrailer.html',
+            controller: "servicioTrailerCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsAction', {
+        url: '/playlistsAction',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsAction.html',
+            controller: "servicioActionCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsCf', {
+        url: '/playlistsCf',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsCf.html',
+            controller: "servicioCfCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsSerial', {
+        url: '/playlistsSerial',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsSerial.html',
+            controller: "servicioSerialCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsDoc', {
+        url: '/playlistsDoc',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsDoc.html',
+            controller: "servicioDocCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsShort', {
+        url: '/playlistsShort',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsShort.html',
+            controller: "servicioShortCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsMusic', {
+        url: '/playlistsMusic',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsMusic.html',
+            controller: "servicioMusicCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsBook', {
+        url: '/playlistsBook',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsBook.html',
+            controller: "servicioBookCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsSoccer', {
+        url: '/playlistsSoccer',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlistsSoccer.html',
+            controller: "servicioSoccerCtrl"
+          }
+        }
+      })
+
+      .state('app.playlistsKid', {
+    url: '/playlistsKid',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlistsKid.html',
+        controller: "servicioKidCtrl"
+      }
+    }
+  })
+
+      .state('app.asdf', {
+        url: '/asdf',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html',
+            //controller: "servicioasdfCtrl"
+          }
+        }
+      })
+
+
+   /* .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })*/
+
+
+  .state('app.single', {
+    url: '/playlistsMovie/:playlistTitulo',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/browse.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  })
+
+
+  angular.module('starter', ['ionic'])
+
+      .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+          if(window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+          }
+        });
+      })
+
+      .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('noMenu', {
+              "url" : "/noMenu",
+              abstract : true,
+              templateUrl : "noMenuState.html"
+            })
+
+            .state("noMenu.simplePage", {
+              "url" : "/simplePage",
+              views : {
+                "noMenuView" : {
+                  templateUrl : "simplePage"
+                }
+              }
+            })
+
+
+            .state("noMenu.simplePage2", {
+              "url" : "/simplePage2",
+              views : {
+                "noMenuView" : {
+                  templateUrl : "simplePage2"
+                }
+              }
+            })
+            .state('app', {
+              url: "/app",
+              abstract: true,
+              templateUrl: "main.html"
+            })
+
+
+            .state('app.state1', {
+              url: "/state1",
+              views: {
+                'menuContent' :{
+                  templateUrl: "state-1-center.html",
+                },
+                'right-menu' : {
+                  templateUrl : "state-1-right.html"
+                }
+              }
+            })
+
+            .state('app.state2', {
+              url: "/state2",
+              views: {
+                'menuContent' :{
+                  templateUrl: "state-2-center.html"
+                },
+                'right-menu' : {
+                  templateUrl : "state-2-right.html"
+                }
+              }
+            })
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/state1');
+      })
+
+      .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+        console.log('MainCtrl');
+
+        $scope.toggleLeft = function() {
+          $ionicSideMenuDelegate.toggleLeft();
+        };
+
+        $scope.data=["JavaScript","Java","Ruby","Python"];
+      })
+
+      .directive('searchBar', [function () {
+        return {
+          scope: {
+            ngModel: '='
+          },
+          require: ['^ionNavBar', '?ngModel'],
+          restrict: 'E',
+          replace: true,
+          template: '<ion-nav-buttons side="right">'+
+          '<div class="searchBar">'+
+          '<div class="searchTxt" ng-show="ngModel.show">'+
+          '<div class="bgdiv"></div>'+
+          '<div class="bgtxt">'+
+          '<input type="text" placeholder="Procurar..." ng-model="ngModel.txt">'+
+          '</div>'+
+          '</div>'+
+          '<i class="icon placeholder-icon" ng-click="ngModel.txt=\'\';ngModel.show=!ngModel.show"></i>'+
+          '</div>'+
+          '</ion-nav-buttons>',
+
+          compile: function (element, attrs) {
+            var icon=attrs.icon
+                || (ionic.Platform.isAndroid() && 'ion-android-search')
+                || (ionic.Platform.isIOS()     && 'ion-ios7-search')
+                || 'ion-search';
+            angular.element(element[0].querySelector('.icon')).addClass(icon);
+
+            return function($scope, $element, $attrs, ctrls) {
+              var navBarCtrl = ctrls[0];
+              $scope.navElement = $attrs.side === 'right' ? navBarCtrl.rightButtonsElement : navBarCtrl.leftButtonsElement;
+
+            };
+          },
+          controller: ['$scope','$ionicNavBarDelegate', function($scope,$ionicNavBarDelegate){
+            var title, definedClass;
+            $scope.$watch('ngModel.show', function(showing, oldVal, scope) {
+              if(showing!==oldVal) {
+                if(showing) {
+                  if(!definedClass) {
+                    var numicons=$scope.navElement.children().length;
+                    angular.element($scope.navElement[0].querySelector('.searchBar')).addClass('numicons'+numicons);
+                  }
+
+                  title = $ionicNavBarDelegate.getTitle();
+                  $ionicNavBarDelegate.setTitle('');
+                } else {
+                  $ionicNavBarDelegate.setTitle(title);
+                }
+              } else if (!title) {
+                title = $ionicNavBarDelegate.getTitle();
+              }
+            });
+          }]
+        };
+      }])
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/inicio');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,202 @@
+angular.module('starter.controllers', [])
+
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
+        // Form data for the login modal
+        $scope.loginData = {};
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
+
+    .controller('servicioMovieCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=peliculasold"
+            , {cache: true})
+            .then(function (response) {
+                $scope.peliculas = {};
+                $scope.peliculas = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioSerialCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=series"
+            , {cache: true})
+            .then(function (response) {
+                $scope.series = {};
+                $scope.series = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioNewCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=novedades"
+            , {cache: true})
+            .then(function (response) {
+                $scope.news = {};
+                $scope.news = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioTrailerCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=trailer"
+            , {cache: true})
+            .then(function (response) {
+                $scope.trailers = {};
+                $scope.trailers = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioActionCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=peliculas"
+            , {cache: true})
+            .then(function (response) {
+                $scope.actions = {};
+                $scope.actions = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioCfCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=cienciaficcion"
+            , {cache: true})
+            .then(function (response) {
+                $scope.cfs = {};
+                $scope.cfs = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioDocCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=documentales"
+            , {cache: true})
+            .then(function (response) {
+                $scope.docs = {};
+                $scope.docs = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioShortCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=cortos"
+            , {cache: true})
+            .then(function (response) {
+                $scope.shorts = {};
+                $scope.shorts = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioMusicCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=musica"
+            , {cache: true})
+            .then(function (response) {
+                $scope.musics = {};
+                $scope.musics = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioBookCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=libros"
+            , {cache: true})
+            .then(function (response) {
+                $scope.books = {};
+                $scope.books = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+    .controller('servicioSoccerCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=futbol"
+            , {cache: true})
+            .then(function (response) {
+                $scope.soccers = {};
+                $scope.soccers = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+
+    .controller('servicioKidCtrl', function ($scope, $http) {
+        $http.get("http://172.16.10.3/playcenter/mediacenter/classes/media.php?op=kids"
+            , {cache: true})
+            .then(function (response) {
+                $scope.kids = {};
+                $scope.kids = response.data;
+                console.log(response.data[0].titulo);
+            });
+
+
+    })
+
+
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+        //console.log($stateParams.playlistTitulo);
+        $scope.titulo = $stateParams.playlistTitulo;
+        console.log($scope.titulo);
+        $scope.url = encodeURI("http://172.16.10.3/playcenter/media/" + $scope.titulo + ".mp4");
+        console.log($scope.url);
+
+    })
+
+    .filter('trusted', ['$sce', function ($sce) {
+        return function (url) {
+            return $sce.trustAsResourceUrl(url);
+        };
+    }]);
