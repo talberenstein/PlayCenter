2291d852b4fe61374230486c2fa450700a90925f
desarrollo <gestsol2016>
2016-11-08 16:02:26 -0300

    services and more services

--- playcenter-app/www/js/services.js
+++ playcenter-app/www/js/services.js
@@ -29,7 +29,7 @@ angular.module('starter.services', [])
                             url_cover: "http://172.16.10.3/playcenter/covers/Batman The Dark Knight Rises",
                             url_trailer: "http://172.16.10.3/playcenter/media/Batman The Dark Knight Rises_trailer.mp4",
                             star_rating: 5,
-                            descripcion: "Director: Christopher Nolan Año: 2012 Duracion: 165min                                                                       Hace ocho años que Batman desapareció, dejando de ser un héroe para convertirse en un fugitivo. Al asumir la culpa por la muerte del fiscal del distrito Harvey Dent, el Caballero Oscuro decidió sacrificarlo todo por lo que consideraba, al igual que el Comisario Gordon, un bien mayor. La mentira funciona durante un tiempo, ya que la actividad criminal de la ciudad de Gotham se ve aplacada gracias a la dura Ley Dent. Pero todo cambia con la llegada de una astuta gata ladrona que pretende llevar a cabo un misterioso plan. Sin embargo, mucho más peligrosa es la aparición en escena de Bane, un terrorista enmascarado cuyos despiadados planes obligan a Bruce a regresar de su voluntario exilio."
+                            descripcion: "Director: Christopher Nolan Año: 2012 Duracion: 165min"
                         },
                         {
                             id: "5",
@@ -38,8 +38,8 @@ angular.module('starter.services', [])
                             url_movie: "http://172.16.10.3/playcenter/media/SinFiltro.mp4",
                             url_cover: "http://172.16.10.3/playcenter/covers/SinFiltro",
                             url_trailer: "http://172.16.10.3/playcenter/media/SinFiltro_trailer.mp4",
-                            star_rating: 2,
-                            descripcion: "Duración: 100 minuntos\n2016\nDirección: Nicolás López\n\nPía, es una agobiada mujer moderna de 37 años que vive en el estresado Santiago del 2015. Día a día es pasada a llevar por su jefe, su novio, su hijastro y su mejor amiga, siendo incapaz de hacer algo al respecto. Ella tiene un dolor constante en el pecho que no la deja vivir y un día, luego de sentir mucho dolor, decide someterse a un milenario tratamiento de acupuntura oriental impartido por un extraño doctor chino. Este tratamiento, sin que ella lo quiera, soltará todo lo que Pía se guardaba dentro y la llevarán por un viaje de liberación personal. Sin embargo, con el tiempo, se dará cuenta que decir todo lo que piensa, no siempre traerá buenas consecuencias. Teniendo así, que aprender a lidiar con sus emociones y pensamientos."
+                            star_rating: 3,
+                            descripcion: "Duración: 100 minuntos\n2016\nDirección: Nicolás López\n\nPía, es una agobiada mujer moderna de 37 años que vive en el estresado Santiago del 2015do incapaz de hacer algo al respecto."
                         }
                     ];
 
